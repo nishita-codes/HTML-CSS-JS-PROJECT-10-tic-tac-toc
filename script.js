92926d554628833ed5a5b15a9923c00e3d6e8d4b
Nishita-codes <nishitapatidar03@gmail.com>
2025-01-09 01:32:20 +0530

    added files

--- script.js
+++ script.js
@@ -24,18 +24,27 @@ const checkWin = ()=>{
         [2,4,6],
     ]
     wins.forEach(e =>{
-       if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "")); 
-       document.querySelector('.Info').innerText = boxtext[e[0]].innerText + "Won";
+       if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "")){ 
        gameover = true;
-       document.querySelector('.images').getElementsByTagName('img')[0].style.width = "56px";
+       document.querySelector('.Info').innerText = boxtext[e[0]].innerText + " Won ";
+       document.querySelector('.images').getElementsByTagName('img')[0].style.width = "200px";
+       boxtext[e[0]].style.color = 'rgb(221, 175, 243)';
+       boxtext[e[1]].style.color = 'rgb(221, 175, 243)';
+       boxtext[e[2]].style.color = 'rgb(221, 175, 243)';
+
+       boxtext[e[0]].style.textShadow = '5px 5px 6px  rgb(221, 175, 243)';
+       boxtext[e[1]].style.textShadow = '5px 5px 6px  rgb(221, 175, 243)';
+       boxtext[e[2]].style.textShadow = '5px 5px 6px  rgb(221, 175, 243)';
+       audiogameover.play(); 
+       }
     })
 }
 
 // Game Logic
-music.play();
+// music.play();
 let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach(element =>{
-     let boxtext = element.querySelectorAll('.boxtext');
+     let boxtext = element.querySelector('.boxtext');
      element.addEventListener('click', ()=>{
         if(boxtext.innerText === ''){
             boxtext.innerText = turn;
@@ -48,4 +57,18 @@ Array.from(boxes).forEach(element =>{
             
         }
      })
+})
+
+// add onclick listener to reset button
+reset.addEventListener('click', ()=>{
+    let boxtexts = document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element =>{
+        element.innerText = ""; //Reset text to none
+        element.style.color = ""; // Reset color to default
+        element.style.textShadow = ""; // Remove any shadow
+    });
+    turn ="X";
+    gameover = false;
+    document.getElementsByClassName('Info')[0].innerText = "Turn for " + turn;
+    document.querySelector('.images').getElementsByTagName('img')[0].style.width = "0px";
 })
